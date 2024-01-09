f1f427e0eb234a1204d28d22058503b4f73f5328
MaToSens <matoosens@gmail.com>
2024-01-09 12:37:06 +0100

    Added initial files

---
+++ CustomPropertyWrapper/CustomPropertyWrapper/CustomPropertyWrapperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomPropertyWrapperApp.swift
+//  CustomPropertyWrapper
+//
+//  Created by MaTooSens on 09/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomPropertyWrapperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
